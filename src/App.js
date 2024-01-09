5e89aa958480eb610085598f51f72152a5e1f588
Daniele <daniele.noto@outlook.com>
2024-01-09 10:52:55 +0100

    scenes fixed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+// App.js
+
+import { createRoot } from 'react-dom/client';
+import React, { useState } from 'react';
+import GothicScene from './GothicScene';
+import RoomScene from './RoomScene';
+import { Button, Navbar,Container } from 'react-bootstrap';
+import "./App.css"
 
 function App() {
+  const [change, setChange] = useState(1); // Initializing state with a value of 1
+
+  const nextScene = () => {
+    setChange(change + 1); // Update the change state to the next value
+  };
+
+  const previousScene = () => {
+    setChange(change - 1); // Update the change state to the previous value
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="root" style={{height: '100vh', width: '100%', overflow: 'hidden'}}>
+      <div className="Navbar">
+        <button style={{marginRight: '10em'}} onClick={previousScene}>Previous</button>
+        <button style={{marginLeft: '10em'}} onClick={nextScene}>Next</button>
+      </div>
+      <div className='row'>
+        <hr className="hr" />
+      </div>
+
+      {change === 1 && <GothicScene />}
+      {change === 2 && <RoomScene />}
     </div>
   );
 }
+
+createRoot(document.getElementById('root')).render(<App />);
 
 export default App;
